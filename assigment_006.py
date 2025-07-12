4359ddfc5cff6c8bf942666271d5f9eba6c8fa63
Hasim Ali <rindhasimali@gmail.com>
2025-07-12 18:19:01 +0500

    Add files via upload

---
+++ assigment_006.py
@@ -0,0 +1,36 @@
+# 1. Characters (Single-character strings)
+char1 = 'A'
+char2 = '9'
+char3 = '#'
+print(char1, char2, char3)
+
+# 2. Strings (Multiple characters)
+name = "Python"
+sentence = "Learning Python is fun!"
+print(name)
+print(sentence)
+
+# 3. String Operations
+
+# (i) Concatenation
+first_name = "Mantashah"
+last_name = "Asim"
+full_name = first_name + " " + last_name
+print(full_name)
+
+# (ii) Repetition
+word = "Ha "
+print(word * 5)
+
+# (iii) Indexing
+word = "Python"
+print(word[0])  # 'P'
+print(word[3])  # 'h'
+
+# (iv) Slicing
+text = "Hello World"
+print(text[0:5])  # 'Hello'
+
+# (v) Length
+text = "Python"
+print(len(text))  # Output: 6
